90a79d0a6232f493b3ea6dd8909d3897653518c3
Jyothigeo <126162684+Jyothigeo@users.noreply.github.com>
2023-02-27 14:36:26 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 st.title("Hi, I am Jyothi Prativadi :wave:")
+st.balloons()
 st.header("Data science intern at innomatics")
 st.write( "A Data enthusiast with strong passion towards delivering object oriented and high standard data driven results that contribute to the organizations value addition"
     )
-st.write("Follow me on:")
+st.write(':red[Follow me on:]')
 st.write(":email: jojeet1512@gmail.com")
 st.write(":link: https://www.linkedin.com/in/jyothi-prativadi-07ab8064/")
 st.write(":link: https://github.com/jyothigeo")
